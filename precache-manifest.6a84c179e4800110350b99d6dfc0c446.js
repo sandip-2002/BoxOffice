a08ebc9e692a8295776c554b33d73965bc350a87
Sandip Roy <sandiproy03062002@gmail..com>
2022-11-11 17:41:26 +0530

    Updates

---
+++ precache-manifest.6a84c179e4800110350b99d6dfc0c446.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50b703af17dbcbcfac591145b6f939da",
+    "url": "/BoxOffice/index.html"
+  },
+  {
+    "revision": "b18df1542bcb2d5d44e3",
+    "url": "/BoxOffice/static/css/main.93052401.chunk.css"
+  },
+  {
+    "revision": "47b5d9282e0107ef5bda",
+    "url": "/BoxOffice/static/js/2.bb8bd143.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/BoxOffice/static/js/2.bb8bd143.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b18df1542bcb2d5d44e3",
+    "url": "/BoxOffice/static/js/main.30de4a80.chunk.js"
+  },
+  {
+    "revision": "2fef80242a692f7d0346",
+    "url": "/BoxOffice/static/js/runtime-main.37dad279.js"
+  }
+]);
